3780ecce8768ccd1ca982804b20e145e9f8121ec
Koki Oyatsu <kaishuu0123@gmail.com>
2017-12-17 17:04:29 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# go-dot
+
+golang dot command(graphviz).
+
+## How to Install
+
+Make sure you have graphviz(libgvc, libcgraph, libcdt) & libpng installed.
+
+```
+$ go get -u github.com/kaishuu0123/go-dot
+```
+
+## How to use
+
+* create sample.dot file
+
+```
+// The graph name and the semicolons are optional
+graph graphname {
+    a -- b -- c;
+    b -- d;
+}
+```
+
+* convert to png
+
+```
+$ go-dot -Tpng sample.dot -o sample.png
+```
+
+* output png preview
+![sample.png](https://github.com/kaishuu0123/go-dot/sample.png)
+
+## DEMO
+
+see examples directory.
+
+## WHY IMPLEMENT THIS
+
+* golang can cross compile.
+* want use cross platform dot command. (and want single binary)
+
+## LICENSE
+
+* Eclipse Public License - v 1.0 (see LICENSE file)
+
+## TODO
+
+* support travis CI
+* Release cross binary
+
+## CONTACTS
+
+* If you have a bug or believe something is not working as expected, please submit a [github issue](https://github.com/kaishuu0123/go-dot).
---
+++ dot.go
@@ -0,0 +1,48 @@
+package main
+
+// #cgo pkg-config: libgvc libcgraph libcdt libpng
+// #include <gvc.h>
+// #include <gvcext.h>
+// #include <time.h>
+// #ifdef HAVE_UNISTD_H
+// #include <unistd.h>
+// #endif
+// lt_symlist_t lt_preloaded_symbols[] = { { 0, 0 } };
+import "C"
+import "os"
+
+func main() {
+  gvc := C.gvContext()
+  // C.GvExitOnUsage = 1;
+  argc := C.int(len(os.Args))
+  argv := make([]*C.char, argc)
+  for i, arg := range os.Args {
+    argv[i] = C.CString(arg)
+  }
+  C.gvParseArgs(gvc, argc, (**C.char)(&argv[0]))
+
+  var prev *C.Agraph_t
+  g := C.gvPluginsGraph(gvc)
+  if g != nil {
+    C.gvLayoutJobs(gvc, g)
+    C.gvRenderJobs(gvc, g)
+  } else {
+    for {
+      g := C.gvNextInputGraph(gvc)
+      if g == nil {
+        break
+      }
+
+      if prev != nil {
+        C.gvFreeLayout(gvc, prev)
+        C.agclose(prev)
+      }
+      C.gvLayoutJobs(gvc, g)
+      C.gvRenderJobs(gvc, g)
+      C.gvFinalize(gvc)
+      prev = g
+    }
+  }
+
+  C.gvFreeContext(gvc)
+}
